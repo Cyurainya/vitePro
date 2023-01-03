95708a8896e20304c815186419c36a42d8067caa
yannisCyu <1047072111@qq.com>
2023-01-03 18:31:33 +0800

    feat: update file

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,11 @@
-import ReactDOM from "react-dom";
+/*
+ * @Author: yannis cyu
+ * @Date: 2023-01-03 10:08:05
+ * @LastEditors: yannis
+ * @LastEditTime: 2023-01-03 18:30:39
+ * @Description: 请填写简介
+ */
+import ReactDOM from "react-dom/client";
 import "@/styles/reset.less";
 import "@/assets/iconfont/iconfont.less";
 import "@/assets/fonts/font.less";
@@ -11,28 +18,14 @@ import { Provider } from "react-redux";
 import { store, persistor } from "@/store";
 import App from "@/App";
 
-// react 17 创建，控制台会报错，暂时不影响使用（菜单折叠时不会出现闪烁）
-ReactDOM.render(
+// react 18 创建（会导致 antd 菜单折叠时闪烁，等待官方修复）
+ReactDOM.createRoot(document.getElementById("root")!).render(
 	// * react严格模式
 	// <React.StrictMode>
 	<Provider store={store}>
 		<PersistGate persistor={persistor}>
 			<App />
 		</PersistGate>
-	</Provider>,
-	// </React.StrictMode>,
-	document.getElementById("root")
+	</Provider>
+	// </React.StrictMode>
 );
-
-// import ReactDOM from "react-dom/client";
-// react 18 创建（会导致 antd 菜单折叠时闪烁，等待官方修复）
-// ReactDOM.createRoot(document.getElementById("root")!).render(
-// 	// * react严格模式
-// 	// <React.StrictMode>
-// 	<Provider store={store}>
-// 		<PersistGate persistor={persistor}>
-// 			<App />
-// 		</PersistGate>
-// 	</Provider>
-// 	// </React.StrictMode>
-// );
